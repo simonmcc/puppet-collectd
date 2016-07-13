cfc82bb881c1761192167e4ef8490eb9d382c936
Simon McCartney <simon.mccartney@hp.com>
2016-07-13 18:44:22 +0100

    GOZ-3020 - enable collectd to graphite on CentOS 6 via carbon_writer
    * This is a fork puppet-collectd circa v3.1.0
    * Drop in the carbon_writer plugin from
      https://github.com/indygreg/collectd-carbon

---
+++ manifests/plugin/carbon_writer.pp
@@ -0,0 +1,36 @@
+# https://github.com/indygreg/collectd-carbon
+class collectd::plugin::write_graphite (
+  $ensure            = present,
+  $graphitehost      = 'localhost',
+  $graphiteport      = 2003,
+  $storerates        = true,
+  $graphiteprefix    = 'collectd.',
+  $graphitepostfix   = undef,
+  $interval          = undef,
+  $escapecharacter   = '_',
+  $alwaysappendds    = false,
+  $protocol          = 'tcp',
+  $separateinstances = false,
+  $logsenderrors     = true,
+) {
+  validate_bool($storerates)
+  validate_bool($separateinstances)
+  validate_bool($logsenderrors)
+
+  file {'/opt/collectd-plugins':
+    ensure => directory,
+  }
+
+  file { '/opt/collectd-plugins/carbon_writer.py':
+    ensure  => present,
+    source  => 'puppet://modules/collectd/carbon_writer.py'
+    require => File['/opt/collectd-plugins'],
+  }
+
+  collectd::plugin {'carbon_writer':
+    ensure   => $ensure,
+    content  => template('collectd/plugin/carbon_writer.conf.erb'),
+    interval => $interval,
+    require  => File['/opt/collectd-plugins/carbon_writer.py'],
+  }
+}
